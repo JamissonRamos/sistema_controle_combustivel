67a2d4ab769459b2806516d79b2995a5ba0c9c13
Jamisson Lira Ramos <missonlira@gmail.com>
2022-05-09 22:13:25 -0300

    atualizando arquivos

--- js/main.js
+++ js/main.js
@@ -1,74 +1,24 @@
-import {animeScroll,addcl,remcl} from "./modules/animations_pages.js";
-import {copiarTexto} from "./modules/copiar_texto.js";
-import {buscaDadosGitHub} from "./modules/meus_projetos.js"
-import {ValidatorCampos} from './modules/validacao_campo.js'
-import {masks} from './modules/masks.js'
+import {ValidatorCampos} from "./modules/validacao_campo.js";
+
+//#region Variveis
 
 let validator = new ValidatorCampos();
-
-//#region Funções
-
-function show_menu_hbg()
-{   
-    //classList => Vai trazer um array com todas as class que tem no elemento com o id selecionado para ser verificado.
-    idNavMenu.classList.forEach(itens => 
-    {
-        if (itens == 'active')
-        {   
-            idSectionMenuHbg.classList.toggle("active");
-            idNavMenu.classList.toggle("active");
-        };
-    });
-}
-
-function formSubmit()
-{
-    document.getElementById('idFrmEnvioEmail').submit();
-}
 
 //#endregion
 
 
-//#region Eventos do html
 
-//Inicialiçando funções
-animeScroll(); 
-buscaDadosGitHub();
+//#region Eventos do HTML
 
-//Evento do menu HBG 
-idSectionMenuHbg.addEventListener("click", () => 
-{   
-    //Foi preciso separa esses comando de add e remover a class active para atender as regras de quando for clicado pelo menu Hbg e for clicado menu normal
+btnCalcular.addEventListener('click', (event) => {   
     
-    idSectionMenuHbg.classList.toggle("active");
-    idNavMenu.classList.toggle("active");
-
-});
-
-//Evento do scroll
-idDivContainer.addEventListener('scroll', (evt) => 
-{
-    // evt.target.scrollTop, verica o top do do id conteiner a distancia que ele se encontra do top da pagina
-    evt.target.scrollTop > 110 ? idDivHome.style.display = "block" : idDivHome.style.display = "none"
- 
-    animeScroll();
- })
-
-idDivCardNome.addEventListener('click', () => { copiarTexto(idSpanCardNome)});
-idDivCardEmail.addEventListener('click', () => { copiarTexto(idSpanCardEmail)});
-idDivCardCelular.addEventListener('click', () => { copiarTexto(idSpanCardCelular)});
-idDivCardEndereco.addEventListener('click', () => { copiarTexto(idSpanCardEndereco)});
-
-
-idButtonEnviarMsg.addEventListener('click', (event) => {   
-
     event.preventDefault(); 
-
-    console.log('clicou no btn de envio de msg box')
-
+    
+    console.log('clicou no btn de calculo dos dados')
+    
     //mandando o meu form para ser valiado
-    validator.validate(idFrmEnvioEmail);
-
+    validator.validate(idFormCadastro);
+    
     let erro = document.querySelectorAll('form .e')
 
     if(erro.length === 0)
@@ -81,24 +31,10 @@ idButtonEnviarMsg.addEventListener('click', (event) => {
     }
 })
 
-//Animações dos inputs ao receber e perder o foco 
-document.querySelectorAll('.input, #idinputEnviarMsg').forEach(input => 
-{
-    input.addEventListener("focus", addcl);
-    input.addEventListener("blur", remcl);
-});
-
-    //Vericar as mascaras dos campos
-document.querySelectorAll('input').forEach($input => {
-    const field = $input.dataset.js
-
-    $input.addEventListener('input', e => {
-        e.target.value = masks[field](e.target.value)
-    }, false)
 
 
-});
+
+
+
 
 //#endregion
-
-window.show_menu_hbg=show_menu_hbg;
---
+++ js/modules/validacao_campo.js
@@ -0,0 +1,130 @@
+export class ValidatorCampos
+{
+    
+    constructor()
+    {
+        this.validations =
+        [
+			'data-required', //Campos obrigatorios
+			'data-only-numbers', //Digitar somente números
+			'data-rend-numbe', //Receber valor de dias da semana
+        ];
+    };/* Fim do constructior */
+
+    validate(form)    
+    {
+        let percorrerValidations = document.querySelectorAll('form .error-validation');
+
+        if(percorrerValidations.length > 0)
+        {
+            this.limparValidations(percorrerValidations);
+        }
+
+        let inputs = form.getElementsByTagName('input');
+
+//         console.log(inputs)
+//         console.log(textareas)
+
+        let arrayInputs = [...inputs]
+       
+//         console.log(arrayInputs)
+
+        arrayInputs.forEach(item =>
+        {
+            for (let i = 0; this.validations.length > i; i++) {
+
+                if(item.getAttribute(this.validations[i]) != null)
+                {
+                    console.log(this.validations[i].replace('data-','').replace('-',''))
+                    console.log(item)
+
+                    let nomeMetodo = this.validations[i].replace('data-','').replace('-','');
+
+                    //console.log(nomeMetodo)
+
+                    let valorAtributo = item.getAttribute(this.validations[i]);
+
+                    this[nomeMetodo](item, valorAtributo)
+                }
+            }
+        }, this);
+    };
+
+    // método para exibir inputs que são necessários
+    required(input)
+    {
+        let inputValue = input.value;
+
+        if(inputValue === '')
+        {
+            let errorMessage = `Opa, Este campo é obrigatório`;
+
+            this.imprimirMsgbox(input, errorMessage);
+        }
+        else if(inputValue <= 0)
+        {
+            let errorMessage = `Ei, Deve preencher com um valor maior que zero (0)`;
+
+            this.imprimirMsgbox(input, errorMessage);
+        }
+}
+
+
+	//Campo pode receber valor de 1 ate 8
+	rendnumbe(input)
+	{
+		let inputValue = input.value
+
+		if(inputValue > 8)
+		{
+			let errorMessage = `Opa, esse campo so pode receber valor de 1 até 8`;
+
+			this.imprimirMsgbox(input, errorMessage);
+		}
+	}
+
+	//Receber somente numero, ',' ou '.'
+	onlynumbers(input) {
+		//Formta a variavel para compara alor recebido do input
+		let re =  /^[0-9.]+$/; ///^[0-9.]+$/
+		//limpa e os espaços e troca as , por .
+		let inputValue = input.value.replace(',','.').trim();
+
+		let errorMessage = `Ei, este campo não aceita letras somente números`;
+
+		if(!re.test(inputValue)) {
+			this.imprimirMsgbox(input, errorMessage);
+		}
+
+	}
+
+
+    imprimirMsgbox(input, msg)
+    {
+        let qtdErros = input.parentNode.querySelector('.error-validation');
+
+        if(qtdErros === null)
+        {
+            let novaDivErro = document.querySelector('.error-validation').cloneNode(true);
+
+            novaDivErro.textContent = msg;
+
+            let inputPai = input.parentNode;
+
+            novaDivErro.classList.remove('template');
+
+            inputPai.appendChild(novaDivErro);
+
+            novaDivErro.classList.add('e');
+
+            // novaDivErro.style.backgroundColor = 'rgba(252, 14, 14, 0.233)';
+            novaDivErro.style.borderLeftColor = 'red';
+        }
+    };
+
+    limparValidations(validations)
+    {
+        validations.forEach(item => {item.remove()})
+    };
+
+};/* Fim da Class */
